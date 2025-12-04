1dd5a63c54d52894300860ffac61d0a6b02b33ee
matisuilvilla <matisuilvilla@gmail.com>
2025-12-04 00:18:14 -0300

    vibecoding error 80mil(tengo que aprender a programar)

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Inventory, Branch, Supplier
+from apps.users.models import Company
 from utils.validators import validar_rut
 
 # Requisito PDF: CRUD de Branches (Sucursales)
@@ -7,38 +8,48 @@ class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branch
         fields = '__all__'
-        read_only_fields = ('company',) # Usuario no elige la compañía, se asigna sola
+       
 
     def create(self, validated_data):
-        # Asignar automáticamente la compañía del usuario creador
         user = self.context['request'].user
-        validated_data['company'] = user.company
+        # Si el usuario tiene empresa, forzamos esa.
+        if user.company:
+            validated_data['company'] = user.company
+        # Si es Super Admin y no mandó empresa, fallará a nivel de modelo o validación previa
         return super().create(validated_data)
 
-# Requisito PDF: CRUD de Suppliers (Proveedores)
 class SupplierSerializer(serializers.ModelSerializer):
     rut = serializers.CharField(validators=[validar_rut])
+    company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all(), required=False)
 
     class Meta:
         model = Supplier
         fields = '__all__'
-        read_only_fields = ('company',) # Importante: read_only
 
     def create(self, validated_data):
-        # Asignar automáticamente la compañía del usuario creador
         user = self.context['request'].user
-        validated_data['company'] = user.company
+        
+        # REGLA DE ORO:
+        # 1. Si es empleado (tiene company), el sistema IMPONE su empresa. Ignora lo que envíe.
+        if user.company:
+            validated_data['company'] = user.company
+        
+        # 2. Si es Super Admin (no tiene company), DEBE haber enviado el campo 'company'.
+        # Si no lo envió, lanzamos error limpio.
+        elif not validated_data.get('company'):
+             raise serializers.ValidationError({"company": "Como Super Admin, debe seleccionar una empresa para este registro."})
+             
         return super().create(validated_data)
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-        read_only_fields = ('company',) 
 
     def create(self, validated_data):
         user = self.context['request'].user
-        validated_data['company'] = user.company
+        if user.company:
+            validated_data['company'] = user.company
         return super().create(validated_data)
 
 class InventorySerializer(serializers.ModelSerializer):
